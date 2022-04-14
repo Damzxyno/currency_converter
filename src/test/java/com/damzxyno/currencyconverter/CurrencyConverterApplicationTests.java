bbbee62f7f3fb7b51064c6d3cadd7effd463c3b3
Decagon <decagon@Decagons-MacBook-Air.local>
2022-04-14 17:08:03 +0100

    Initialised

---
+++ src/test/java/com/damzxyno/currencyconverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damzxyno.currencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
